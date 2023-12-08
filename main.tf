6a78ac612b84ec93531fa8289d262d37dee372f1
Obaidul Haque Sarker <aupo37@gmail.com>
2023-12-08 22:06:58 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,21 @@ variable "db_engine_version" {
   default = ["13.13", "15.4"]                 # provide each instance database version
 }
 
+variable "security_group_ids" {
+  type    = list(string)
+  default = ["sg-0f4b39ca7a90e9826", "sg-0f4b39ca7a90e9826"]  # Replace with your actual security group ID, must have existed it
+}
+
+variable "db_subnet_group_names" {
+  type    = list(string)
+  default = ["default", "default"]  # Replace with your actual DB subnet group name, must have existed it
+}
+
+variable "db_parameter_group_names" {
+  type    = list(string)
+  default = ["default.postgres13", "default.postgres15"]  # Replace with your actual DB parameters group name, must have existed it
+}
+
 resource "aws_db_instance" "postgresql_rds" {
   count                  = var.instance_count
   identifier             = var.db_instance_identifiers[count.index]
@@ -38,6 +53,9 @@ resource "aws_db_instance" "postgresql_rds" {
   password               = "asd#^*02jkas5238343"              # database password
   publicly_accessible   = false                               # Modify as needed
   skip_final_snapshot    = true
+  vpc_security_group_ids = [var.security_group_ids[count.index]]  
+  db_subnet_group_name   = var.db_subnet_group_names[count.index]
+  parameter_group_name   = var.db_parameter_group_names[count.index]
 
   # Configure other settings as needed
 
